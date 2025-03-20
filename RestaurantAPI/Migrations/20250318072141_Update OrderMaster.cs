0368699a797f637d4b9e4ebd817368ee6652cb6c
TaoGiaKhanh147 <taogiakhanh@gmail.com>
2025-03-20 12:19:59 +0700

    restaurant_app_backend_.net

---
+++ RestaurantAPI/Migrations/20250318072141_Update OrderMaster.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RestaurantAPI.Migrations
+{
+    public partial class UpdateOrderMaster : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "PMethod",
+                table: "OrderMasters",
+                type: "nvarchar(10)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(10)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "OrderNumber",
+                table: "OrderMasters",
+                type: "nvarchar(75)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(75)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "PMethod",
+                table: "OrderMasters",
+                type: "nvarchar(10)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(10)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "OrderNumber",
+                table: "OrderMasters",
+                type: "nvarchar(75)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(75)",
+                oldNullable: true);
+        }
+    }
+}
---
+++ RestaurantAPI/Program.cs
@@ -0,0 +1,35 @@
+using Microsoft.EntityFrameworkCore;
+using RestaurantAPI.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext <RestaurantDbContext> (option =>
+    option.UseSqlServer(builder.Configuration.GetConnectionString("DevConnection")));
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.UseCors(builder => builder
+       .AllowAnyHeader()
+       .AllowAnyMethod()
+       .AllowAnyOrigin()
+    ); ;
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
