a4d9a2410e154d0838f32a4627fee5fef8674f11
taogiakhanh147 <140130133+taogiakhanh147@users.noreply.github.com>
2025-05-07 15:44:38 +0700

    update deploy

--- RestaurantAPI/Program.cs
+++ RestaurantAPI/Program.cs
@@ -3,36 +3,38 @@ using RestaurantAPI.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
+// Add services
 builder.Services.AddControllers();
-builder.Services.AddDbContext <RestaurantDbContext> (option =>
-    option.UseSqlServer(builder.Configuration.GetConnectionString("DevConnection")));
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddDbContext<RestaurantDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DevConnection")));
+
+// Add CORS services
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontendApp",
+        builder =>
+        {
+            builder.WithOrigins("https://restaurant-app-liart-gamma.vercel.app")
+                   .AllowAnyHeader()
+                   .AllowAnyMethod();
+        });
+});
+
+// Swagger
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-app.UseCors(builder => builder
-       .AllowAnyHeader()
-       .AllowAnyMethod()
-       .AllowAnyOrigin()
-    );
+// Use CORS middleware with named policy
+app.UseCors("AllowFrontendApp");
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
+// Swagger
 app.UseSwagger();
 app.UseSwaggerUI();
 
+// Routing and Authorization
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
-
